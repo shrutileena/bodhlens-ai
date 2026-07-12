471fdd45019a4856db8adb3c1be7a393c394e800
shruti agarwal <shrutileena@gmail.com>
2026-07-12 23:11:57 +0530

    Jwt auth validation completed

--- backend/documind-ai/src/main/java/com/documind/documind_ai/security/JwtService.java
+++ backend/documind-ai/src/main/java/com/documind/documind_ai/security/JwtService.java
@@ -2,10 +2,12 @@ package com.documind.documind_ai.security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -30,5 +32,36 @@ public class JwtService {
 				.expiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(getSigningKey())
 				.compact();
+	}
+	
+	// reading payload inside the token
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.verifyWith((javax.crypto.SecretKey) getSigningKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+	
+	private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+	
+	public boolean isTokenValid(String token, String email) {
+		String username = extractUsername(token);
+		return username.equals(email) && !isTokenExpired(token);
 	}
 }
--- backend/documind-ai/src/main/java/com/documind/documind_ai/security/SecurityConfig.java
+++ backend/documind-ai/src/main/java/com/documind/documind_ai/security/SecurityConfig.java
@@ -2,17 +2,25 @@ package com.documind.documind_ai.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+	
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http.csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers("/api/health",
 							"/api/auth/register",
@@ -20,7 +28,8 @@ public class SecurityConfig {
 					.permitAll()
 					.anyRequest()
 					.authenticated())
-			.httpBasic(Customizer.withDefaults());
+//			.httpBasic(Customizer.withDefaults())
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
